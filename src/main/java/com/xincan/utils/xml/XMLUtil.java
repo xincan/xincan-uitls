cb152d8fa98dd6455692f27597f371b0e463bad6
alittlexincan <alittlexincan@sina.com>
2018-08-17 14:28:43 +0800

    添加静态xml生成

--- src/main/java/com/xincan/utils/xml/XMLUtil.java
+++ src/main/java/com/xincan/utils/xml/XMLUtil.java
@@ -2,7 +2,6 @@ package com.xincan.utils.xml;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
