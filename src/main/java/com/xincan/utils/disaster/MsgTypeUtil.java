f91137522acc3e3f6b41fb719e2f5e2fbb1cc352
alittlexincan <alittlexincan@sina.com>
2018-09-13 13:11:24 +0800

    添加一键发布类型转换信息

--- src/main/java/com/xincan/utils/disaster/MsgTypeUtil.java
+++ src/main/java/com/xincan/utils/disaster/MsgTypeUtil.java
@@ -15,4 +15,13 @@ public class MsgTypeUtil {
         if(type.equals("Ack")) return "确认";
         return null;
     }
+
+    public static String parseOneType(int type){
+        if(type==0) return  "短期预报";
+        if(type==1) return "中期预报";
+        if(type==2) return "长期预报";
+        if(type==3) return "气象专题专报";
+        if(type==4) return "重大气象专题专报";
+        return null;
+    }
 }
